958d9e744445bf7b0828c16aeee264fd2915f212
Belal Khan <belalkhan.dev@gmail.com>
2023-04-20 18:19:42 +0600

    some more changes added

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function() {
+    return response()->json([
+        'service' => 'API',
+        "status" => "Ok",
+        "version" => "1.0.0",
+        "message" => "Hello! I am mc-track",
+    ]);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
